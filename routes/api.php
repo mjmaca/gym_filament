d58dba84681feda11188fd33b649ab29977e3be9
MICHAEL\macam <macamichaelgeorge@gmail.com>
2024-06-21 10:02:34 +0800

    add login and profile route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Member;
+use App\Models\Expense;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +15,39 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/login', function (Request $request) {
+    $credentials = $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    if (!Auth::attempt($credentials)) {
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
+
+    $user = Auth::user();
+    $token = $user->createToken('token')->plainTextToken;
+
+    return response()->json(['token' => $token]);
 });
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Routes that require authentication
+    Route::get('/member/{email}', function ($email) {
+        return Member::where('email', $email)->first();
+    });
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
+
+
+
+
+
+
+
+
+
+
