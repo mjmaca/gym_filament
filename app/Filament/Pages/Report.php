aae415aecf4ea8729f6f966e7166c98adfecfd67
stepheljobs <stephelgeorgemaca@gmail.com>
2024-06-28 20:50:04 +0800

    added blade for report table

--- app/Filament/Pages/Report.php
+++ app/Filament/Pages/Report.php
@@ -10,6 +10,9 @@ use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Section;
 use Filament\Pages\Dashboard\Concerns\HasFiltersForm;
 
+use App\Models\MembershipPlan;
+use App\Models\Payment;
+
 class Report extends Page
 {
     use HasFiltersForm;
@@ -17,6 +20,16 @@ class Report extends Page
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
     protected static string $view = 'filament.pages.report';
     protected static ?int $navigationSort = 3;
+
+    public $activeTab = "membership";
+
+    public $tabList = [
+        'membership' => 'Gym Membership',
+        'access' => 'Gym Access',
+        'coaching' => 'Coaching',
+        'summary' => 'Summary',
+    ];
+
     public function filtersForm(Form $form): Form
     {
         return $form
@@ -33,5 +46,27 @@ class Report extends Page
                     ])
                     ->columns(3),
             ]);
+    }
+
+    public function activateButton($btnName)
+    {
+        $this->activeTab = $btnName;
+    }
+
+    public function getMembershipPlan() {
+        $respose = [];
+
+        $memberships = MembershipPlan::all();        
+        foreach ($memberships as $membership) {
+            $respose[] = [
+                'membership_name' => $membership->type,
+                'price' => $membership->price,
+                'client_count' => Payment::where('gym_membership_type', $membership->type)->count(),
+                'discount' => Payment::where('gym_membership_type', $membership->type)->sum('gym_membership_discount'),
+                'amount' => Payment::where('gym_membership_type', $membership->type)->sum('amount')
+            ];
+        }
+
+        return $respose;
     }
 }
--- resources/views/filament/pages/report.blade.php
+++ resources/views/filament/pages/report.blade.php
@@ -9,4 +9,49 @@
         @livewire(\App\Filament\Widgets\MemberOverview::class, ['filters' => $this->filters])
         @livewire(\App\Filament\Widgets\SalesOverview::class, ['filters' => $this->filters])
     </div>
+
+    <div>
+        {{-- {{ print_r($tabList) }}
+            {{ print_r($activeTab) }} --}}
+        <div class="flex space-x-4">
+            @foreach ($tabList as $tab)
+                <button wire:click="activateButton('{{ $tab }}')"
+                    class="{{ $activeTab === $tab ? 'bg-red-500' : 'bg-blue-500' }} hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{{ ucfirst($tab) }}</button>
+            @endforeach
+        </div>
+
+        <table class="min-w-full divide-y divide-gray-200">
+            <thead class="bg-gray-50">
+                <tr>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Gym
+                        Membership</th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Price
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">No of
+                        Clients</th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Discounts
+                    </th>
+                    <th scope="col"
+                        class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Amount
+                    </th>
+                </tr>
+            </thead>
+
+            <tbody class="bg-white divide-y divide-gray-200">
+                @foreach ($this->getMembershipPlan() as $plan)
+                    <tr>
+                        <td class="px-6 py-4 whitespace-nowrap">{{ $plan['membership_name'] }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap">{{ $plan['price'] }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap">{{ $plan['client_count'] }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap">{{ $plan['discount'] }}</td>
+                        <td class="px-6 py-4 whitespace-nowrap">{{ $plan['amount'] }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </x-filament::page>
