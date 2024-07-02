74935394c71216a09bf9a51063da3a250ba0b1df
MICHAEL\macam <macamichaelgeorge@gmail.com>
2024-07-02 22:05:32 +0800

    Change navigationLabel in Attendance

--- app/Filament/Resources/AttendanceResource.php
+++ app/Filament/Resources/AttendanceResource.php
@@ -7,20 +7,19 @@ use App\Models\Attendance;
 use App\Models\Member;
 use Filament\Forms;
 use Filament\Forms\Form;
-use Filament\Pages\Actions\ButtonAction;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Filament\Forms\Components\Section;
-// use Filament\Forms\Components\Actions\Action;
-use Filament\Actions\Action;
-use Filament\Navigation;
+
 
 class AttendanceResource extends Resource
 {
     protected static ?string $model = Attendance::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationLabel  = "Daily Attendances";
     public $selectedMember;
 
 
@@ -45,37 +44,22 @@ class AttendanceResource extends Resource
                             ->columns(2)
                             ->hidden(fn($get) => !$get('selectedMember'))
                             ->schema([
-                                Forms\Components\Placeholder::make('membership_id')
-                                    ->label('Membership ID')
-                                    ->content(fn($get) => $get('selectedMember.membership_id') ?? 'Non Member'),
                                 Forms\Components\Placeholder::make('full_name')
                                     ->label('Full Name')
-                                    ->content(fn($get) => $get('selectedMember.full_name') ?? 'N/A')
-                                    ->hidden(fn($get) => !$get('selectedMember')),
+                                    ->content(fn($get) => $get('selectedMember.full_name')),
+                                Forms\Components\Placeholder::make('email')
+                                    ->label('Email')
+                                    ->content(fn($get) => $get('selectedMember.email')),
+                                Forms\Components\Placeholder::make('mobile_number')
+                                    ->label('Mobile Number')
+                                    ->content(fn($get) => $get('selectedMember.mobile_number')),
+                                Forms\Components\Placeholder::make('branch_location')
+                                    ->label('Branch Location')
+                                    ->content(fn($get) => $get('selectedMember.branch_location'))
                             ]),
                     ]),
-
-                // Forms\Components\Actions::make([
-                //     Forms\Components\Actions\Action::make('Create')
-                //     ->action(function ($get) {
-                //             $attendance = new Attendance();
-                //             $attendance->member_id = $get('selectedMember.id');
-                //             $attendance->save();
-                //     })
-                //     ->disabled(fn($get) => !$get('selectedMember')),
-                // ]),
             ]);
-        // ->mutateFormDataBeforeCreate(function ($get) {
-        //     $attendance = new Attendance();
-        //     $attendance->membership_id = $get('selectedMember.membership_id');
-        //     $attendance->member_id = $get('selectedMember.id');
-        //     $attendance->branch_location = $get('selectedMember.branch_location');
-        //     $attendance->fullname = $get('selectedMember.fullname');
-        //     $attendance->is_guest = $get('selectedMember.is_guest');
 
-        //     logger('Data:::'. $attendance);
-        //     return $attendance;
-        // });
     }
 
     public static function table(Table $table): Table
@@ -99,7 +83,6 @@ class AttendanceResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
